812970eb860cdc738d0609f20f4b7fd472aeeeab
Joan López <joan@netzstrategen.com>
2019-06-19 17:39:24 +0200

    Added backend label format configuration to woocommerce settings.

--- src/Admin.php
+++ src/Admin.php
@@ -39,12 +39,76 @@ class Admin {
    * @implements admin_init
    */
   public static function init() {
+    if (function_exists('register_field_group')) {
+      static::register_acf();
+    }
+
+    // Adds prices label PDF document generation action.
     add_action('post_action_label', __CLASS__ . '::post_action_label');
-    add_action('woocommerce_product_options_pricing', __CLASS__ . '::woocommerce_product_options_pricing', 1);
+    // Adds prices label format and print controls to simple product prices section.
+    add_action('woocommerce_product_options_pricing', __CLASS__ . '::woocommerce_product_options_pricing', 9);
+    // Adds prices label format and print controls to product variation prices section.
     add_action('woocommerce_variation_options_pricing', __CLASS__ . '::woocommerce_variation_options_pricing', 10, 3);
+
+    // Adds a configuration section to woocommerce products settings tab.
+    add_filter('woocommerce_get_sections_products', __CLASS__ . '::woocommerce_get_sections_products');
+    add_filter('woocommerce_get_settings_products', __CLASS__ . '::woocommerce_get_settings_products', 10, 2);
+
+    // Enqueues plugin scripts.
+    add_filter('admin_enqueue_scripts', __CLASS__ . '::admin_enqueue_scripts');
+  }
+
+  public static function register_acf() {
+    $products_attributes = [];
+    foreach (wc_get_attribute_taxonomies() as $attribute) {
+      $products_attributes[$attribute->attribute_name] = $attribute->attribute_label;
+    }
+    acf_add_local_field_group([
+      'key' => 'acf_group_' . Plugin::PREFIX . '_products_attributes',
+      'title' => __('Prices label', Plugin::L10N),
+      'fields' => [
+        [
+          'key' => 'acf_' . Plugin::PREFIX . '_products_attributes',
+          'label' => __('Attributes to print', Plugin::L10N),
+          'name' => 'acf_' . Plugin::PREFIX . '_products_attributes',
+          'type' => 'select',
+          'instructions' => __('Select the attributes to print in the product prices label.', Plugin::L10N),
+          'required' => 0,
+          'conditional_logic' => 0,
+          'choices' => $products_attributes,
+          'default_value' => [],
+          'allow_null' => 0,
+          'multiple' => 1,
+          'ui' => 1,
+          'ajax' => 1,
+          'return_format' => 'array',
+          'placeholder' => '',
+        ],
+      ],
+      'location' => [
+        [
+          [
+            'param' => 'taxonomy',
+            'operator' => '==',
+            'value' => 'product_cat',
+          ],
+        ],
+      ],
+      'menu_order' => 0,
+      'position' => 'normal',
+      'style' => 'default',
+      'label_placement' => 'top',
+      'instruction_placement' => 'label',
+      'hide_on_screen' => '',
+      'active' => 1,
+      'description' => '',
+    ]);
+
   }
 
   /**
+   * Adds prices label PDF document generation action.
+   *
    * @implements post_action_{$action}
    */
   public static function post_action_label($post_id) {
@@ -63,7 +127,7 @@ class Admin {
       $labelFormat = explode('|', $_GET['format']);
     }
     else {
-      $labelFormat = explode('|', get_option(Plugin::PREFIX . '-format'));
+      $labelFormat = explode('|', get_option(Plugin::PREFIX . '-format', array_keys(static::PDF_LABEL_FORMATS)[0]));
     }
 
     $regular_price = $product->get_regular_price();
@@ -102,20 +166,65 @@ class Admin {
     Pdf::render($data, $labelFormat[0], $labelFormat[1]);
   }
 
+  /**
+   * Adds prices label format and print controls to simple product prices section.
+   *
+   * @implements woocommerce_product_options_pricing
+   */
   public static function woocommerce_product_options_pricing() {
-    $link = add_query_arg('action', 'label');
-    echo '<p><a class="button" href="' . $link . '" target="blank">' . __(static::PDF_LABEL_PRINT_BUTTON, Plugin::L10N) . '</a></p>';
+    $labelFormatDefault = get_option(Plugin::PREFIX . '-format');
+    $link = add_query_arg(
+      [
+        'post' => get_the_ID(),
+        'action' => 'label',
+        'format' => $labelFormatDefault,
+      ],
+      get_admin_url() . 'post.php'
+    );
+    static::displayPricesLabelFormatsSelect(static::PDF_LABEL_FORMATS, $labelFormatDefault);
+    static::displayPricesLabelPrintButton($link, __(static::PDF_LABEL_PRINT_BUTTON, Plugin::L10N), static::PDF_LABEL_FORMATS);
   }
 
+  /**
+   * Adds prices label format and print controls to product variation prices section.
+   *
+   * @implements woocommerce_variation_options_pricing
+   */
   public static function woocommerce_variation_options_pricing($loop, $variation_data, $variation) {
+    $labelFormatDefault = get_option(Plugin::PREFIX . '-format');
     $link = add_query_arg(
       [
         'post' => $variation->ID,
         'action' => 'label',
+        'format' => $labelFormatDefault,
       ],
       get_admin_url() . 'post.php'
     );
-    echo '<p><a class="button" href="' . $link . '" target="blank">' . __(static::PDF_LABEL_PRINT_BUTTON, Plugin::L10N) . '</a></p>';
+    static::displayPricesLabelFormatsSelect(static::PDF_LABEL_FORMATS, $labelFormatDefault);
+    static::displayPricesLabelPrintButton($link, __(static::PDF_LABEL_PRINT_BUTTON, Plugin::L10N), static::PDF_LABEL_FORMATS);
+  }
+
+  /**
+   * Displays a select list with the available prices label formats.
+   *
+   * @param array $labelFormats
+   *   Available sizes, orientation and font size for the prices label.
+   * @param string $labelFormatDefault
+   *   Default label format.
+   */
+  public static function displayPricesLabelFormatsSelect($labelFormats, $labelFormatDefault='') {
+    if (empty($labelFormatDefault)) {
+      $labelFormatDefault = array_keys($labelFormats)[2];
+    }
+    $selected = ' selected="selected"';
+    ?>
+    <p class="form-field _price_label_print">
+      <select id="<?= Plugin::PREFIX ?>-format" style="margin-right: 8px;">
+      <?php foreach ($labelFormats as $key => $format): ?>
+        <option value="<?= $key ?>" <?= $key === $labelFormatDefault ? $selected : '' ?>><?= $format ?></option>
+      <?php endforeach; ?>
+      </select>
+<?php
   }
 
   /**
@@ -176,28 +285,53 @@ class Admin {
    *   Product for which attributes should be retrieved.
    *
    * @return array
-   *   List of attributes of the product.
+   *   List of attributes for the product prices label.
    */
-  public static function getProductAttributes(\WC_Product $product) {
-    if ($parent_id = $product->get_parent_id()) {
-      $product = wc_get_product($parent_id);
-    }
+  public static function getProductPricesLabelAttributes(\WC_Product $product) {
     $attributes = [];
+    $query = [
+      'orderby' => 'name',
+      'order' => 'ASC',
+      'fields' => 'ids',
+    ];
 
-    foreach ($product->get_attributes() as $attribute) {
-      if (!$attribute->get_visible()) {
-        continue;
-      }
-      if ($attribute->is_taxonomy()) {
-        $values = [];
-        $attribute_taxonomy = $attribute->get_taxonomy_object();
-        $name = $attribute_taxonomy->attribute_label;
-        $values = wc_get_product_terms($product->get_id(), $attribute->get_name(), [
+    $product_id = $product->get_type() === 'variation' ? $product->get_parent_id() : $product->get_id();
+    $product_categories = wc_get_product_terms($product_id, 'product_cat', $query);
+
+    if ($product_categories) {
+      $category_attributes = static::getAttributesByProductCategoryId($product_categories[0]);
+      foreach ($category_attributes as $category_attribute) {
+        $values = wc_get_product_terms($product_id, 'pa_' . $category_attribute['value'], [
           'fields' => 'names',
         ]);
+        if ($values) {
+          $attributes[$category_attribute['label']] = $values[0];
+        }
       }
-      $attributes[$name] = $values;
     }
+
+    return $attributes;
+  }
+
+  /**
+   * Retrieves the attributes to print on the prices label for the given category.
+   *
+   * Traverses up recursively the product categories list until it finds a
+   * a category with assigned product attributes.
+   *
+   * @param int $category_id
+   *   Unique category identifier.
+   *
+   * @return array
+   *   Category attributes.
+   */
+  public static function getAttributesByProductCategoryId($category_id) {
+    if (!$attributes = get_field('acf_' . Plugin::PREFIX . '_products_attributes', 'product_cat_' . $category_id)) {
+      if ($parent_category_id = get_term_by('id', $category_id, 'product_cat')->parent) {
+        $attributes = static::getAttributesByProductCategoryId($parent_category_id);
+      }
+    };
+
     return $attributes;
   }
 
@@ -253,6 +387,21 @@ class Admin {
     echo $qrCode->writeString();
 
     return ob_get_clean();
+  }
+
+  /**
+   * Enqueues plugin scripts.
+   *
+   * @implements wp_enqueue_scripts
+   */
+  public static function admin_enqueue_scripts() {
+    if (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) {
+      $script = '/assets/scripts/main.js';
+    }
+    else {
+      $script = '/dist/scripts/main.min.js';
+    }
+    wp_enqueue_script(Plugin::PREFIX, Plugin::getBaseUrl() . $script, ['jquery'], Plugin::getGitVersion(), TRUE);
   }
 
 }

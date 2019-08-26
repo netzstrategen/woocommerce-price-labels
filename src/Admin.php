a2c6cf537e8228677f5389993f9d611a55dcf030
Joan López <joan@netzstrategen.com>
2019-08-26 10:09:53 +0200

    Fixup! Fixed only first product attribute value was printed.
    Co-Authored-By: Daniel Kudwien <daniel@netzstrategen.com>

--- src/Admin.php
+++ src/Admin.php
@@ -314,7 +314,6 @@ class Admin {
       $values = wc_get_product_terms($product_id, 'pa_' . $category_attribute['value'], [
         'fields' => 'names',
       ]);
-
       if ($values) {
         $attributes[$category_attribute['label']] = implode(', ', $values);
       }

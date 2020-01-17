62a9b5450b5f23ab00d68c8e9ca1247fb14ec24e
Joan López <joan@netzstrategen.com>
2020-01-17 14:43:08 +0100

    fixup! Fixed price format for label templates.

--- src/Admin.php
+++ src/Admin.php
@@ -28,13 +28,6 @@ class Admin {
   ];
 
   /**
-   * Price suffix, instead of ',00'.
-   *
-   * @string
-   */
-  const PRICE_SUFFIX = ',-';
-
-  /**
    * Size of QR code to include in the PDF label document.
    *
    * @integer
@@ -144,13 +137,12 @@ class Admin {
       $discount_percentage = 0;
     }
 
+    $price_suffix = ',-';
     $formatted_regular_price = wc_price($regular_price, ['price_format' => '%1$s&nbsp;%2$s']);
-    // Replace ",00".
-    $formatted_regular_price = preg_replace('@,00@', static::PRICE_SUFFIX, $formatted_regular_price);
-
+    // Replace ",00" with ",-".
+    $formatted_regular_price = preg_replace('@,00@', $price_suffix, $formatted_regular_price);
     $formatted_sale_price = wc_price($sale_price, ['price_format' => '%1$s&nbsp;%2$s']);
-    // Replace ",00".
-    $formatted_sale_price = preg_replace('@,00@', static::PRICE_SUFFIX, $formatted_sale_price);
+    $formatted_sale_price = preg_replace('@,00@', $price_suffix, $formatted_sale_price);
 
     $data = [
       'orientation' => $labelFormat[1],

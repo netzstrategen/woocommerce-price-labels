edf61099fe743338dec99d456b722f051a6fc3cb
Alin Istrate <alin.istrate@olivestudio.net>
2022-06-21 13:11:23 +0300

    Changed design for sale price label PDF.

--- templates/label-portrait.php
+++ templates/label-portrait.php
@@ -25,8 +25,8 @@ namespace Netzstrategen\WooCommercePriceLabels;
       font-style: normal;
     }
     <?php
-      include Plugin::getBasePath() . '/dist/styles/label.min.css';
-      include Plugin::getBasePath() . '/dist/styles/label-portrait.min.css';
+    include Plugin::getBasePath() . '/dist/styles/label.min.css';
+    include Plugin::getBasePath() . '/dist/styles/label-portrait.min.css';
     ?>
   </style>
 </head>
@@ -37,36 +37,33 @@ namespace Netzstrategen\WooCommercePriceLabels;
   <div class="container">
     <h1 class="label__title <?= $paper_size ?>"><?= __('Exhibit', Plugin::L10N); ?></h1>
     <h2 class="product-name"><?= $title ?></h2>
+    <div class="prices">
+      <div class="prices__sale">
+        <p class="prices__amount price--current"><?= $formatted_sale_price ?> <span class="price__currency"><?= $currency_symbol ?></span></p>
+        <p class="prices__amount price--regular"><?= __('Instead of', Plugin::L10N) ?>: <?= $formatted_regular_price  ?> <span class="price__currency"><?= $currency_symbol ?></span> <span class="price__label">(UVP)</span></p>
+      </div>
+    </div>
     <div class="details">
-      <?php if ($attributes): ?>
+      <?php
+      if ($attributes) : ?>
         <table class="attributes">
-          <?php foreach ($attributes as $label => $value): ?>
+          <?php foreach ($attributes as $label => $value) : ?>
           <tr>
             <td class="attribute__label"><?= $label ?></td>
-            <td class="attribute__value"><?= $max_row_chars && strlen($value)>$max_row_chars ? sprintf('%s...', substr($value, 0, $max_row_chars)) : $value ?></td>
+            <td class="attribute__value"><?= $max_row_chars && strlen($value) > $max_row_chars ? sprintf('%s...', substr($value, 0, $max_row_chars)) : $value ?></td>
           </tr>
           <?php endforeach; ?>
         </table>
       <?php endif; ?>
-    </div>
-    <div class="qr-code">
-      <img class="qr-code__image" src="<?= $qr_code ?>" />
-      <p class="qr-code__help">
-        <?= __('Scan this QR code for more Infos directly in our online shop.', Plugin::L10N) ?>
-      </p>
-    </div>
-    <div class="prices">
-      <div class="prices__discount">
-        <p class="discount__title"><?= __('Now', Plugin::L10N) ?></p>
-        <p class="discount__value">-<?= $discount_percentage ?>%</p>
-      </div>
-      <div class="prices__sale">
-        <p class="prices__amount price--current"><?= $formatted_sale_price ?> <span class="price__currency"><?= $currency_symbol ?></span></p>
-        <p class="prices__amount price--regular"><?= __('Instead of', Plugin::L10N) ?>: <?= $formatted_regular_price  ?> <span class="price__currency"><?= $currency_symbol ?></span></p>
+      <div class="qr-code">
+        <p class="qr-code__help">
+          <?= __('Alles info:', Plugin::L10N) ?>
+        </p>
+        <img class="qr-code__image" src="<?= $qr_code ?>" />
       </div>
     </div>
     <div class="footer">
-      <p><?= __('These are collection prices in EUR incl. VAT. Shipping possible against surcharge. Errors without prior sale reserve.', Plugin::L10N) ?></p>
+      <p><strong><?= __('These are collection prices in EUR incl. VAT.', Plugin::L10N) ?></strong> <span class="footer__additional-text"><?= __('Shipping possible against surcharge. Errors without prior sale reserve.', Plugin::L10N) ?></span></p>
     </div>
   </div>
 </body>
